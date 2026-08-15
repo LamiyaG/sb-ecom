4638d3fe08c3c0fd6803f187702875b1687c41ed
Lamiya Gonsalves <lamiya1925@gmail.com>
2026-08-15 09:35:59 -0400

    Product APIs.

---
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Product;
+import com.ecommerce.project.payload.ProductDTO;
+import com.ecommerce.project.payload.ProductResponse;
+
+public interface ProductService {
+    ProductDTO addProduct(Product product, Long categoryId);
+
+    ProductResponse getAllProducts();
+
+    ProductResponse searchByCategory(Long categoryId);
+
+    ProductResponse searchByKeyword(String keyword);
+}
